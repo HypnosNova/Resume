adfc21e68dd4752b04633252207f5ade49ab7dc4
HypnosNova <601670314@qq.com>
2015-08-26 17:03:25 +0800

    add

---
+++ js/language/chinese.js
@@ -0,0 +1,9 @@
+var chinese={
+	idiom:"梦创造的真实世界——Hypnos Nova",
+	hoster:"自我简介",
+	resume:"我的简历",
+	experience:"工作经验",
+	production:"作品展示",
+	blog:"我的博客",
+	other:"其他"
+}
---
+++ js/language/english.js
@@ -0,0 +1,9 @@
+english={
+	idiom:"Dreams make real world——Hypnos Nova",
+	hoster:"About Myself",
+	resume:"My Resume",
+	experience:"Experiences",
+	production:"Works Gallery",
+	blog:"My Blogs",
+	other:"Other"
+}
